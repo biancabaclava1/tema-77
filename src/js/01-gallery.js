dd616e2805b31f8396acdea361a87b1d9b0454d9
BiancaBaclava <149661109+biancabaclava1@users.noreply.github.com>
2024-04-27 14:01:29 +0300

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+
+function createGalleryItemsMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`;
+    })
+    .join('');
+}
+
+galleryList.addEventListener('click', selectImage);
+
+function selectImage(e) {
+  if (e.target.nodeName !== 'IMG') return;
+
+  e.preventDefault();
+
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}">`,
+    {
+      onShow: instance => {
+        document.addEventListener('keyup', escapeCloseFunction);
+      },
+      onClose: instance => {
+        document.removeEventListener('keyup', escapeCloseFunction);
+      },
+    }
+  );
+
+  function escapeCloseFunction(e) {
+    if (e.key === 'Escape') instance.close();
+  }
+
+  instance.show();
+}
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector('.gallery');
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
 
+
+galleryList.insertAdjacentHTML('beforeend', galleryItemsMarkup);
 console.log(galleryItems);
+
+
+function createGalleryItemsMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+  <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+  </li>`;
+    })
+    .join('');
+}
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+console.log(lightbox);
